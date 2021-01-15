ba3d90c413d97fd8019552f7dca62f5af8e6505f
Fernando <fleiva@trazalog.com>
2021-01-15 12:54:32 -0300

    Multiempresa

--- models/Articulos.php
+++ models/Articulos.php
@@ -16,7 +16,7 @@ class Articulos extends CI_Model
 
 	public function obtenerXTipo($tipo)
 	{
-		$resource = "/articulos/tipo/$tipo";
+		$resource = "/articulos/tipo/$tipo/".empresa();
         $url = REST_ALM . $resource;
         return wso2($url);
 	}
