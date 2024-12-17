2aa3a555591e69983b28085090075b48c64b6fad
Gera Ramos <geramos55@gmail.com>
2024-12-17 15:17:04 -0300

    TICA: ocultar campo stock de desplegable

--- views/lotes/list_new.php
+++ views/lotes/list_new.php
@@ -181,13 +181,22 @@ input:checked+.slider:before {
                         <!-- /.form-group -->
                         <!-- ARTICULO -->
                         <div class="form-group col-xs-12 col-sm-4 col-md-4 col-lg-4">
-                            <label>Artículo</label>
+                            <label>Artículo 123</label>
                             <input list="articulos" id="inputarti" name="artBarCode" class="form-control" placeholder="Seleccionar Articulo" onchange="getItem(this)" autocomplete="off">
                             <div class="input-group">
                                 <datalist id="articulos">
-                                    <?php foreach($items as $o)
+                                    <?php 
+                                    $usuario = $this->session->userdata();
+                                    foreach($items as $o)
                                     {
-                                        echo  "<option value='".$o->codigo."' data-json='".$o->json."' class=form-control'>".$o->descripcion." | Stock: ".$o->stock."</option>";
+                                        // Verifica si el usuario pertenece al grupo "Tierras_de_Capayan"
+                                        if ($usuario['groupBpm'] == "Tierras_de_Capayan") {
+                                            // Muestra el artículo sin stock
+                                            echo "<option value='" . $o->codigo . "' data-json='" . $o->json . "' class='form-control'>" . $o->descripcion . "</option>";
+                                        } else {
+                                            // Muestra el artículo con stock
+                                            echo "<option value='" . $o->codigo . "' data-json='" . $o->json . "' class='form-control'>" . $o->descripcion . " | Stock: " . $o->stock . "</option>";
+                                        }
                                         unset($o->json);
                                     }
                                     ?>
