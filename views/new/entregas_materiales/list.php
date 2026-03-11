f84ef462850457b092b53c40c424b9060d02cd61
Pablo Marrelli <pmarrelli@trazalog.com>
2026-03-11 10:45:44 -0300

    impresion entrega de materiales
    Generar Comprobante de Entrega - logo configurable en core.tablas
    - tabla = logo_impresion_entrega_materiales
    - valor = url por ejemplo (imagenes/trazalog/logo_trazalog.png)

--- views/new/entregas_materiales/list.php
+++ views/new/entregas_materiales/list.php
@@ -1,4 +1,3 @@
-
 <div class="box box-primary">
     <div class="box-header with-border">
         <h3 class="box-title">Entrega Materiales</h3>
@@ -6,34 +5,35 @@
     <div class="box-body table-responsive">
         <table id="entregas" class="table table-bordered table-striped table-hover">
             <thead>
-               
-                    <th width="10%">Acciones</th>
-                    <th width="10%">N° de Pedido</th>
-                    <th width="10%">N° de Entrega</th>
-                    <th width="10%" class="<?php echo (!viewOT ? "hidden" : null) ?>">Ord.Trabajo</th>
-                    <th class="text-center" width="10%">Fecha</th>
-                    <th>N° Comprobante</th>
-                    <th>Entregado</th>
-                    <th width="12%">Estado Ped.</th>
-               
+
+                <th width="10%">Acciones</th>
+                <th width="10%">N° de Pedido</th>
+                <th width="10%">N° de Entrega</th>
+                <th width="10%" class="<?php echo (!viewOT ? "hidden" : null) ?>">Ord.Trabajo</th>
+                <th class="text-center" width="10%">Fecha</th>
+                <th>N° Comprobante</th>
+                <th>Entregado</th>
+                <th width="12%">Estado Ped.</th>
+
             </thead>
             <tbody>
                 <?php
-                    foreach ($list as $o) {
-                        echo '<tr data-id='.$o['enma_id'].' data-pema="'.$o['pema_id'].'" data-json=\''.json_encode($o).'\'>'; 
-                        echo '<td>';
-                        echo '<i class="fa fa-fw fa-search text-light-blue btn-buscar" style="cursor: pointer; margin:2px" onclick="ConsultarEntrega(this)"title="Consultar"></i> ';
-                        echo '<i class="fa fa-fw  fa-battery text-light-blue btn-estado" style="cursor: pointer; margin:2px"onclick="EstadoPedido(this)" title="Estado Pedido"></i> ';
-                        echo '</td>';
-                        echo '<td class="text-center">'.bolita($o['pema_id'],'blue').'</td>';
-                        echo '<td class="text-center">'.bolita($o['enma_id'],'aqua').'</td>';
-                        echo '<td class="text-center '.(!viewOT ? "hidden" : null).'">'.bolita($o['ortr_id'],'orange').'</td>';
-                        echo '<td class="text-center">'.fecha($o['fecha']).'</td>';
-                        echo '<td>'.$o['comprobante'].'</td>';
-                        echo '<td>'.$o['solicitante'].'</td>';
-                        echo '<td class="text-center" style="cursor: pointer;">'.estadoPedido($o['estado']).'</td>';
-                        echo '</tr>';
-                    }
+                foreach ($list as $o) {
+                    echo '<tr data-id=' . $o['enma_id'] . ' data-pema="' . $o['pema_id'] . '" data-json=\'' . json_encode($o) . '\'>';
+                    echo '<td>';
+                    echo '<i class="fa fa-fw fa-search text-light-blue btn-buscar" style="cursor: pointer; margin:2px" onclick="ConsultarEntrega(this)"title="Consultar"></i> ';
+                    echo '<i class="fa fa-fw  fa-battery text-light-blue btn-estado" style="cursor: pointer; margin:2px"onclick="EstadoPedido(this)" title="Estado Pedido"></i> ';
+                    echo '<i class="fa fa-fw  fa-print text-light-blue btn-imprimir" style="cursor: pointer; margin:2px"onclick="ImprimirEntrega(this)" title="Imprimir Entrega"></i> ';
+                    echo '</td>';
+                    echo '<td class="text-center">' . bolita($o['pema_id'], 'blue') . '</td>';
+                    echo '<td class="text-center">' . bolita($o['enma_id'], 'aqua') . '</td>';
+                    echo '<td class="text-center ' . (!viewOT ? "hidden" : null) . '">' . bolita($o['ortr_id'], 'orange') . '</td>';
+                    echo '<td class="text-center">' . fecha($o['fecha']) . '</td>';
+                    echo '<td>' . $o['comprobante'] . '</td>';
+                    echo '<td>' . $o['solicitante'] . '</td>';
+                    echo '<td class="text-center" style="cursor: pointer;">' . estadoPedido($o['estado']) . '</td>';
+                    echo '</tr>';
+                }
                 ?>
             </tbody>
         </table>
@@ -41,19 +41,19 @@
 </div><!-- /.box -->
 
 <script>
-//DataTable($('#entregas'));
+    //DataTable($('#entregas'));
 
-//Funcion de datatable para extencion de botones exportar
-//excel, pdf, copiado portapapeles e impresion
+    //Funcion de datatable para extencion de botones exportar
+    //excel, pdf, copiado portapapeles e impresion
 
-$(document).ready(function() {
-    $('#entregas').DataTable({
-        responsive: true,
-        language: {
-            url: '<?php base_url() ?>lib/bower_components/datatables.net/js/es-ar.json' //Ubicacion del archivo con el json del idioma.
-        },
-        dom: 'lBfrtip',
-        buttons: [{
+    $(document).ready(function () {
+        $('#entregas').DataTable({
+            responsive: true,
+            language: {
+                url: '<?php base_url() ?>lib/bower_components/datatables.net/js/es-ar.json' //Ubicacion del archivo con el json del idioma.
+            },
+            dom: 'lBfrtip',
+            buttons: [{
                 //Botón para Excel
                 extend: 'excel',
                 exportOptions: {
@@ -97,54 +97,126 @@ $(document).ready(function() {
                 filename: 'Entrega Materiales',
                 text: '<button class="btn btn-default ml-2 mb-2 mb-2 mt-3">Imprimir <i class="fa fa-print mr-1"></i></button>'
             }
-        ]
+            ]
+        });
     });
-});
 
-function ConsultarEntrega(e)
-{
-    var tr = $(e).closest('tr');
-    var id = $(tr).data('id');
-    var json = JSON.parse(JSON.stringify($(tr).data('json')));
-    rellenarCabecera(json);
-    $.ajax({
-        type: 'GET',
-        url: 'index.php/<?php echo ALM ?>new/Entrega_Material/detalle?id='+id,
-        success: function(result) {
-            var tabla = $('#modal_detalle_entrega table');
-    
-            $(tabla).find('tbody').html('');
-            result.forEach(e => {
-                $(tabla).append(
-                    '<tr>' +
-                    '<td>' + e.barcode + '</td>' +
-                    '<td>' + e.descripcion + '</td>' +
-                    '<td>' + e.lote + '</td>' +
-                    '<td>' + e.deposito + '</td>' +
-                    '<td class="text-center">' + e.cantidad + '</td>' +
-                    '</tr>'
-                );
-            });
-         
-            $('#modal_detalle_entrega').modal('show');
-           
-        },
-        error: function(result) {
-            alert('Error');
-        },
-        dataType: 'json'
-    });
-}
+    function ConsultarEntrega(e) {
+        wo();
+        var tr = $(e).closest('tr');
+        var id = $(tr).data('id');
+        var json = JSON.parse(JSON.stringify($(tr).data('json')));
+        rellenarCabecera(json);
+        $.ajax({
+            type: 'GET',
+            url: 'index.php/<?php echo ALM ?>new/Entrega_Material/detalle?id=' + id,
+            success: function (result) {
+                var tabla = $('#modal_detalle_entrega table');
 
-function rellenarCabecera(json) {
-    $('#modal_detalle_entrega .enma_id').val(json.enma_id);
-    $('#modal_detalle_entrega .pema_id').val(json.pema_id);
-    $('#modal_detalle_entrega .orden').val(json.ortr_id);
-    $('#modal_detalle_entrega .comprobante').val(json.comprobante);
-    $('#modal_detalle_entrega .fecha').val(json.fecha);
-    $('#modal_detalle_entrega .entregado').val(json.solicitante);
-    $('#modal_detalle_entrega .estado').val(json.estado);
-}
+                $(tabla).find('tbody').html('');
+                result.forEach(e => {
+                    $(tabla).append(
+                        '<tr>' +
+                        '<td>' + e.barcode + '</td>' +
+                        '<td>' + e.descripcion + '</td>' +
+                        '<td>' + e.lote + '</td>' +
+                        '<td>' + e.deposito + '</td>' +
+                        '<td class="text-center">' + e.cantidad + '</td>' +
+                        '</tr>'
+                    );
+                });
+
+                $('#modal_detalle_entrega').modal('show');
+                wc();
+            },
+            error: function (result) {
+                alert('Error');
+            },
+            dataType: 'json'
+        });
+    }
+
+    function ImprimirEntrega(e) {
+        wo();
+        var tr = $(e).closest('tr');
+        var id = $(tr).data('id');
+        var json = JSON.parse(JSON.stringify($(tr).data('json')));
+        rellenarCabeceraImpresion(json);
+        $.ajax({
+            type: 'GET',
+            url: 'index.php/<?php echo ALM ?>new/Entrega_Material/detalle?id=' + id,
+            success: function (result) {
+                var tabla = $('#modal_detalle_entrega_impresion table');
+
+                // Asignar logo dinámico si existe
+                if (result.logo && result.logo.data && result.logo.data[0] && result.logo.data[0].valor) {
+                    $('#logo_remito').attr('src', result.logo.data[0].valor);
+                } else {
+                    $('#logo_remito').attr('src', 'imagenes/trazalog/logo_trazalog.png');
+                }
+
+                $(tabla).find('tbody').html('');
+                result.detalles.forEach(e => {
+                    $(tabla).append(
+                        '<tr>' +
+                        '<td>' + e.barcode + '</td>' +
+                        '<td>' + e.descripcion + '</td>' +
+                        '<td>' + e.lote + '</td>' +
+                        '<td>' + e.deposito + '</td>' +
+                        '<td class="text-center">' + e.cantidad + '</td>' +
+                        '</tr>'
+                    );
+                });
+
+                $('#modal_detalle_entrega_impresion').modal('show');
+                wc();
+            },
+            error: function (result) {
+                alert('Error');
+            },
+            dataType: 'json'
+        });
+    }
+
+
+    function rellenarCabecera(json) {
+        $('#modal_detalle_entrega .enma_id').val(json.enma_id);
+        $('#modal_detalle_entrega .pema_id').val(json.pema_id);
+        $('#modal_detalle_entrega .orden').val(json.ortr_id);
+        $('#modal_detalle_entrega .comprobante').val(json.comprobante);
+        $('#modal_detalle_entrega .fecha').val(json.fecha);
+        $('#modal_detalle_entrega .entregado').val(json.solicitante);
+        $('#modal_detalle_entrega .estado').val(json.estado);
+    }
+
+    function rellenarCabeceraImpresion(json) {
+        $('#modal_detalle_entrega_impresion .comprobanteEntrega').html(json.enma_id);
+        $('#modal_detalle_entrega_impresion .enma_id_impresion').val(json.enma_id);
+        $('#modal_detalle_entrega_impresion .pema_id_impresion').val(json.pema_id);
+        $('#modal_detalle_entrega_impresion .orden_impresion').val(json.ortr_id);
+        $('#modal_detalle_entrega_impresion .comprobante_impresion').val(json.comprobante);
+        $('#modal_detalle_entrega_impresion .fecha_impresion').val(json.fecha);
+        $('#modal_detalle_entrega_impresion .entregado_impresion').val(json.solicitante);
+        $('#modal_detalle_entrega_impresion .estado_impresion').val(json.estado);
+    }
+
+    //impresion del remito
+    function imprimirEntrega() {
+        var base = "<?php echo base_url() ?>";
+        $('#modal_detalle_entrega_impresion').printThis({
+            debug: false,
+            importCSS: true,
+            importStyle: true,
+            pageTitle: "TRAZALOG TOOLS",
+            printContainer: true,
+            loadCSS: base + "lib/bower_components/bootstrap/dist/css/bootstrap.min.css",
+            copyTagClasses: true,
+            printDelay: 4000,
+            afterPrint: function () {
+            },
+            base: base
+        });
+    }
 </script>
 
 
@@ -218,6 +290,89 @@ function rellenarCabecera(json) {
 </div> <!-- /.modal fade -->
 <!-- / Modal -->
 
+<!-- Modal impresion-->
+<div class="modal fade" id="modal_detalle_entrega_impresion" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+    <div class="modal-dialog " role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close hidden-print" data-dismiss="modal" aria-label="Close"><span
+                        aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title hidden-print" id="myModalLabel"><span id="modalAction"
+                        class="fa fa-print text-light-blue"></span> Imprimir Detalle Pedido Materiales</h4>
+                <br>
+                <div class="row">
+                    <div class="col-xs-8 col-md-8">
+                        <h3 style="margin-top:-10px;"><strong>Comprobante de entrega</strong></h3>
+                        <h3 style=""><strong>N° <span class="comprobanteEntrega"></span></strong></h3>
+                    </div>
+                    <div class="col-xs-4 col-md-4">
+                        <img src="imagenes/trazalog/logo_trazalog.png" id='logo_remito'
+                            style="max-width: 150px; margin-left: 30%;">
+                    </div>
+                </div>
+
+
+                <div class="row">
+                    <br>
+                    <div class="col-xs-12 col-sm-6 col-lg-4">
+                        <label for="">Entrega:</label>
+                        <input class="form-control enma_id_impresion" type="text" value="???" readonly>
+                    </div>
+                    <div class="col-xs-12 col-sm-6 col-lg-4">
+                        <label for="">Pedido:</label>
+                        <input class="form-control pema_id_impresion" type="text" value="???" readonly>
+                    </div>
+                    <div class="col-xs-12 col-sm-6 col-lg-4 <?php echo (!viewOT ? "hidden" : null) ?>">
+                        <label for="">Orden de Trabajo:</label>
+                        <input class="form-control orden_impresion" type="text" value="???" readonly>
+                    </div>
+                    <div class="col-xs-12 col-sm-6 col-lg-4">
+                        <label for="">Comprobante:</label>
+                        <input class="form-control comprobante_impresion" type="text" value="???" readonly>
+                    </div><br>
+                    <div class="col-xs-12 col-sm-6 col-lg-4">
+                        <label for="">Fecha:</label>
+                        <input class="form-control fecha_impresion" type="text" value="???" readonly>
+                    </div>
+                    <div class="col-xs-12 col-sm-6 col-lg-4">
+                        <label for="">Entregado a:</label>
+                        <input class="form-control entregado_impresion" type="text" value="???" readonly>
+                    </div>
+                    <div class="col-xs-12 col-sm-6 col-lg-4">
+                        <label for="">Estado:</label>
+                        <input class="form-control estado_impresion" type="text" value="???" readonly>
+                    </div>
+                </div>
+            </div> <!-- /.modal-header  -->
+            <div class="modal-body">
+                <div class="row">
+                    <div class="col-xs-12">
+                        <table class="table table-bordered table-striped table-hover">
+                            <thead>
+                                <tr>
+                                    <th>Artículo</th>
+                                    <th>Descripción</th>
+                                    <th>N° Lote</th>
+                                    <th>Depósito</th>
+                                    <th>Cantidad</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <!--TABLE BODY -->
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div> <!-- /.modal-body -->
+            <div class='modal-footer hidden-print'>
+                <button type='button' class='btn btn-default' data-dismiss="modal">Cancelar</button>
+                <button type='button' class='btn btn-primary' onclick='imprimirEntrega()'>Imprimir</button>
+            </div> <!-- /.modal footer -->
+        </div> <!-- /.modal-content -->
+    </div> <!-- /.modal-dialog modal-lg -->
+</div> <!-- /.modal fade -->
+<!-- / Modal -->
+
 <?php
-    $this->load->view(ALM.'new/pedidos_materiales/estado_pedido');            
+$this->load->view(ALM . 'new/pedidos_materiales/estado_pedido');
 ?>
